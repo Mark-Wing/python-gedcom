bdabcf031bbcff66069c3498d641d62d9d6b1169
Mark-Wing <102595942+Mark-Wing@users.noreply.github.com>
2025-04-12 16:48:44 -0400

    Add files via upload
    New

---
+++ gedcom/utilities/reports/locations.py
@@ -0,0 +1,94 @@
+
+# Python GEDCOM Parser
+#
+# Copyright (C) 2025 Mark Wing (mark @ markwing.net)
+# Copyright (C) 2018 Damon Brodie (damon.brodie at gmail.com)
+# Copyright (C) 2018-2019 Nicklas Reincke (contact at reynke.com)
+# Copyright (C) 2016 Andreas Oberritter
+# Copyright (C) 2012 Madeleine Price Ball
+# Copyright (C) 2005 Daniel Zappala (zappala at cs.byu.edu)
+# Copyright (C) 2005 Brigham Young University
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+#
+# Further information about the license: http://www.gnu.org/licenses/gpl-2.0.html
+
+import re
+
+"""Table to Map States to Abbreviations Courtesy https://gist.github.com/Quenty/74156dcc4e21d341ce52da14a701c40c"""
+STATE_NAME_TO_ABBREV = {    
+
+    # States
+    'Alabama': 'AL',
+    'Montana': 'MT',
+    'Alaska': 'AK',
+    'Nebraska': 'NE',
+    'Arizona': 'AZ',
+    'Nevada': 'NV',
+    'Arkansas': 'AR',
+    'New Hampshire': 'NH',
+    'California': 'CA',
+    'New Jersey': 'NJ',
+    'Colorado': 'CO',
+    'New Mexico': 'NM',
+    'Connecticut': 'CT',
+    'New York': 'NY',
+    'Delaware': 'DE',
+    'North Carolina': 'NC',
+    'Florida': 'FL',
+    'North Dakota': 'ND',
+    'Georgia': 'GA',
+    'Ohio': 'OH',
+    'Hawaii': 'HI',
+    'Oklahoma': 'OK',
+    'Idaho': 'ID',
+    'Oregon': 'OR',
+    'Illinois': 'IL',
+    'Pennsylvania': 'PA',
+    'Indiana': 'IN',
+    'Rhode Island': 'RI',
+    'Iowa': 'IA',
+    'South Carolina': 'SC',
+    'Kansas': 'KS',
+    'South Dakota': 'SD',
+    'Kentucky': 'KY',
+    'Tennessee': 'TN',
+    'Louisiana': 'LA',
+    'Texas': 'TX',
+    'Maine': 'ME',
+    'Utah': 'UT',
+    'Maryland': 'MD',
+    'Vermont': 'VT',
+    'Massachusetts': 'MA',
+    'Virginia': 'VA',
+    'Michigan': 'MI',
+    'Washington': 'WA',
+    'Minnesota': 'MN',
+    'West Virginia': 'WV',
+    'Mississippi': 'MS',
+    'Wisconsin': 'WI',
+    'Missouri': 'MO',
+    'Wyoming': 'WY',
+}
+
+def multiple_replace(lookup, text):
+    """Perform substituions that map strings in the lookup table to valuees (modification from https://stackoverflow.com/questions/15175142/how-can-i-do-multiple-substitutions-using-regex-in-python)"""
+    # re.IGNORECASE flags allows provides case insensitivity (i.e. matches New York, new york, NEW YORK, etc.)
+    regex = re.compile(r'\b(' + '|'.join(lookup.keys()) + r')\b', re.IGNORECASE)
+
+
+    # For each match, look-up corresponding value in dictionary and peform subsstituion
+    # we convert match to title to capitalize first letter in each word
+    return regex.sub(lambda mo: lookup[mo.string.title()[mo.start():mo.end()]], text) 
