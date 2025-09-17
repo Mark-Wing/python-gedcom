837a5e9d7b5ac87ce939e0c8bef7283c381bee38
Mark-Wing <102595942+Mark-Wing@users.noreply.github.com>
2025-09-17 06:46:31 -0400

    Added new methods
    Added new methods: get_marriages_data, get_divorces_data, get_relationship_data, and get_divorces

--- gedcom/element/family.py
+++ gedcom/element/family.py
@@ -2,6 +2,7 @@
 
 # Python GEDCOM Parser
 #
+# Copyright (C) 2022-2025 Mark Wing (mark @ markwing.net)
 # Copyright (C) 2018 Damon Brodie (damon.brodie at gmail.com)
 # Copyright (C) 2018-2019 Nicklas Reincke (contact at reynke.com)
 # Copyright (C) 2016 Andreas Oberritter
@@ -39,3 +40,92 @@ class FamilyElement(Element):
 
     def get_tag(self):
         return gedcom.tags.GEDCOM_TAG_FAMILY
+    
+    def get_children(self):
+        """Returns a list of the children in the family
+
+        :rtype: list
+        """
+        return self.get_elements_by_tag(gedcom.tags.GEDCOM_TAG_CHILD)
+        
+    def get_husbands(self):
+        """Returns a list of the husbands in the family
+
+        :rtype: list
+        """
+        return self.get_elements_by_tag(gedcom.tags.GEDCOM_TAG_HUSBAND)
+        
+    def get_wives(self):
+        """Returns a list of the wives in the family
+
+        :rtype: list
+        """
+        return self.get_elements_by_tag(gedcom.tags.GEDCOM_TAG_WIFE)
+        
+    def get_marriages(self):
+        """Returns a list of the marriage in the family
+
+        :rtype: list
+        """
+        return self.get_elements_by_tag(gedcom.tags.GEDCOM_TAG_MARRIAGE)
+
+    def get_divorces(self):
+        """Returns a list of the divorces in the family
+
+        :rtype: list
+        """
+        return self.get_elements_by_tag(gedcom.tags.GEDCOM_TAG_DIVORCE)
+    
+    def get_marriages_data(self):
+        """Returns a list of the marriage in the family
+
+        :rtype: list
+        """
+        return self.get_relationship_data(relationship_type=gedcom.tags.GEDCOM_TAG_MARRIAGE)
+
+    def get_divorces_data(self):
+        """Returns a list of the marriage in the family
+
+        :rtype: list
+        """
+        return self.get_relationship_data(relationship_type=gedcom.tags.GEDCOM_TAG_DIVORCE)
+    
+    def get_relationship_data(self, relationship_type=gedcom.tags.GEDCOM_TAG_MARRIAGE):
+        """Returns a list of marriages of an individual formatted as a tuple (`str` spouse, `str` date, `str` place, `list` sources)
+
+        :type relationship_type: string
+
+        :rtype: tuple
+        """
+        relationships = []
+
+        for marriage in self.get_elements_by_tag(relationship_type):
+            date = ''
+            place = ''
+            sources = []
+
+            for relationship_data in marriage.get_child_elements():
+                if relationship_data.get_tag() == gedcom.tags.GEDCOM_TAG_DATE:
+                    date = relationship_data.get_value()
+                if relationship_data.get_tag() == gedcom.tags.GEDCOM_TAG_PLACE:
+                    place = relationship_data.get_value()
+                if relationship_data.get_tag() == gedcom.tags.GEDCOM_TAG_SOURCE:
+                    sources.append(relationship_data)
+                                
+            relationships.append((date, place, sources))
+        
+        return relationships
+    
+    def get_elements_by_tag(self, tag):
+        """Returns a list of the child elements by tag in the family
+
+        :rtype: list
+        """
+        result = []
+
+        for child in self.get_child_elements():
+            if child.get_tag() == tag:
+                result.append(child)
+
+        return result
+        
--- tests/element/test_family.py
+++ tests/element/test_family.py
@@ -1,9 +1,84 @@
 from gedcom.element.element import Element
 from gedcom.element.family import FamilyElement
 import gedcom.tags
-
+from gedcom.parser import Parser
 
 def test_initialization():
     family_element = FamilyElement(level=-1, pointer="", tag=gedcom.tags.GEDCOM_TAG_FAMILY, value="")
     assert isinstance(family_element, Element)
     assert isinstance(family_element, FamilyElement)
+    
+def test_get_children():
+    
+    parser = Parser()
+    parser.parse_file('tests/files/Coolidge.ged')
+
+    criteria = "surname=Coolidge:birth=1872"
+    individual = parser.find_person(criteria)
+
+    families = parser.get_families(individual)
+
+    children = families[0].get_children()
+    
+    assert(children[0].get_value() == '@I20@')
+    assert(children[1].get_value() == '@I21@')
+        
+def test_get_husbands():
+    
+    parser = Parser()
+    parser.parse_file('tests/files/Coolidge.ged')
+
+    criteria = "surname=Coolidge:birth=1872"
+    individual = parser.find_person(criteria)
+
+    families = parser.get_families(individual)
+    
+    husbands = families[0].get_husbands()
+    
+    assert(husbands[0].get_value() == '@I16@')
+    
+def test_get_wives():
+    
+    parser = Parser()
+    parser.parse_file('tests/files/Coolidge.ged')
+
+    criteria = "surname=Coolidge:birth=1872"
+    individual = parser.find_person(criteria)
+
+    families = parser.get_families(individual)
+    
+    wives = families[0].get_wives()
+    
+    assert(wives[0].get_value() == '@I19@')
+
+def test_get_marriages():
+    
+    parser = Parser()
+    parser.parse_file('tests/files/Coolidge.ged')
+
+    criteria = "surname=Coolidge:birth=1845"
+    individual = parser.find_person(criteria)
+
+    families = parser.get_families(individual)
+    
+    marriages = families[0].get_marriages()
+    
+    assert(marriages[0].get_child_value_by_tag('DATE') == '6 MAY 1868')
+    assert(marriages[0].get_child_value_by_tag('PLAC') == 'Plymouth, Windsor, Vermont, USA')
+
+def test_get_marriages_data():
+    
+    parser = Parser()
+    parser.parse_file('tests/files/Coolidge.ged')
+
+    criteria = "surname=Coolidge:birth=1845"
+    individual = parser.find_person(criteria)
+
+    families = parser.get_families(individual)
+    
+    marriages = families[0].get_marriages_data()
+    
+    date, place, source = marriages[0]
+    
+    assert(date == '6 MAY 1868')
+    assert(place == 'Plymouth, Windsor, Vermont, USA')
